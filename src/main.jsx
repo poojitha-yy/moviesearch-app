12acce6a447d6de50c402147d1351bfe52ab799c
poojitha-yy <poojithapelluru46@gmail.com>
2026-03-14 11:09:40 +0530

    new commit to change Browser Router

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,9 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
 /*createRoot(document.getElementById("root")).render(
