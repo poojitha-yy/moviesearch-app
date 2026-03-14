5f2342c1131054acb6ef7d064f66e8467d6eaa5c
poojitha-yy <poojithapelluru46@gmail.com>
2026-03-14 11:17:15 +0530

    Changed BrowserRouter to HashRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import React from "react";
-//import { BrowserRouter } from "react-router-dom";
+//import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
 /*createRoot(document.getElementById("root")).render(
